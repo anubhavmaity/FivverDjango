b62599d6aa417894d085ca7d47f6e5dd696ffab1
anubhav.maity <anubhav.maity@verse.in>
2017-03-18 08:00:31 +0530

    change setting.py

--- fixxer/settings.py
+++ fixxer/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fixxerapp',
-    'social_django',
+    'social.apps.django_app.default',
 ]
 
 MIDDLEWARE = [
